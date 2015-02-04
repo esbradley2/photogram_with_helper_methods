7384fd3ad3341bd129157f86cfaf4f96f47a2875
Raghu Betina <rbetina@github.com>
2015-02-04 16:03:53 -0600

    Switched to RESTful routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,18 @@ Rails.application.routes.draw do
   get("/", { :controller => "photos", :action => "index" })
 
   # Routes to CREATE photos
-  get("/photos/new",       { :controller => "photos", :action => "new_form" })
-  get("/create_photo",     { :controller => "photos", :action => "create_row" })
+  get("/photos/new",          { :controller => "photos", :action => "new" })
+  post("/photos",             { :controller => "photos", :action => "create" })
 
   # Routes to READ photos
-  get("/photos",           { :controller => "photos", :action => "index" })
-  get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/photos",              { :controller => "photos", :action => "index" })
+  get("/photos/:id",          { :controller => "photos", :action => "show" })
 
   # Routes to UPDATE photos
-  get("/photos/:id/edit",  { :controller => "photos", :action => "edit_form" })
-  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
+  get("/photos/:id/edit",     { :controller => "photos", :action => "edit" })
+  patch("/photos/:id",        { :controller => "photos", :action => "update" })
 
   # Route to DELETE photos
-  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+  delete("/photos/:id",       { :controller => "photos", :action => "destroy" })
+
 end
